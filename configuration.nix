b1f02bdac39c2e4c7af8741410607c80368671a4
Erwan <erwan.legrand49@gmail.com>
2024-03-01 23:41:27 +0900

    fix nixos configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -12,21 +12,10 @@ in
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-
-  boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
-    # Dual booting using grub.
-    loader = {
-      grub = {
-        enable = true;
-        devices = ["nodev"];
-        efiSupport = true;
-        useOSProber = true;
-	configurationLimit = 5;
-      };
-      timeout = 10;
-    };
-  };
+  
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = user; # Define your hostname.
   # Pick only one of the below networking options.
@@ -51,14 +40,21 @@ in
   services = {
     xserver = {
       enable = true;
+      
+      desktopManager = {
+        xterm.enable = false;
+      };
+
       displayManager = {
         defaultSession = "none+i3";
       };
+
       windowManager.i3 = {
         enable = true;
         extraPackages = with pkgs; [
           dmenu
           i3status
+          i3lock
         ];
       };
     };
@@ -86,55 +82,7 @@ in
       enable = true;
       powerOnBoot = true;
     };
-
-    # Enable OpenGL
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
-
-
-    nvidia = {
-      # Modesetting is required.
-      modesetting.enable = true;
-
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      # Enable this if you have graphical corruption issues or application crashes after waking
-      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-      # of just the bare essentials.
-      powerManagement.enable = false;
-
-      # Fine-grained power management. Turns off GPU when not in use.
-      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-      powerManagement.finegrained = false;
-
-      # Use the NVidia open source kernel module (not to be confused with the
-      # independent third-party "nouveau" open source driver).
-      # Support is limited to the Turing and later architectures. Full list of 
-      # supported GPUs is at: 
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-      # Only available from driver 515.43.04+
-      # Currently alpha-quality/buggy, so false is currently the recommended setting.
-      open = false;
-
-      # Enable the Nvidia settings menu,
-      # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
-
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-      prime = {
-	# Make sure to use the correct Bus ID values for your system!
-	amdgpuBusId = "PCI:5:0:0";
-	nvidiaBusId = "PCI:1:0:0";
-      };
-    };  
   };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
@@ -184,6 +132,8 @@ in
   
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  
+  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
