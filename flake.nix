560f398331ed9c586de075b2ddd57c64eeb89ea0
Erwan <erwan.legrand49@gmail.com>
2024-03-12 17:17:55 +0900

    home.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    lib = nixpkgs.lib;
-  in {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      asus-laptop = lib.nixosSystem {
-        inherit system;
-        modules = [ 
-            ./hosts/asus-laptop/configuration.nix ];
+      asus-laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/asus-laptop/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.erwan = import ./users/erwan/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
       };
     };
   };
