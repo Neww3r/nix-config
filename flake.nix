95fc884166a25101088eb54bf818e2c5d4ff0346
Erwan <erwan.legrand49@gmail.com>
2024-03-18 22:09:56 +0900

    install the overlay for alacritty-theme

--- flake.nix
+++ flake.nix
@@ -5,22 +5,24 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    alacritty-theme.url = "github:alexghr/alacritty-theme.nix";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       asus-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          # overlays
+          ({ config, pkgs, ... }: {
+            nixpkgs.overlays = [ inputs.alacritty-theme.overlays.default ];
+          })
           ./hosts/asus-laptop/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.erwan = import ./users/erwan/home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
           }
         ];
       };
