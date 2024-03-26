0a43137ead1a64bcbcbe46551e7d5c1695c0fd9d
Erwan <erwan.legrand49@gmail.com>
2024-03-26 15:29:14 +0900

    fetch wallpaper from internet

--- home-modules/stylix/default.nix
+++ home-modules/stylix/default.nix
@@ -1,4 +1,7 @@
+{ pkgs, ... }:
 {
-  stylix.image = ~/Pictures/wallpapers/wallpaper.png;
-  stylix.polarity = "dark";
+  stylix.image = pkgs.fetchurl {
+    url = "https://get.wallhere.com/photo/anime-natural-light-landscape-forest-Studio-Ghibli-Karigurashi-no-Arrietty-1873503.jpg";
+    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  };
 }
