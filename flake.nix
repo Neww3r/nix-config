a6f430bf0f45744650713f29b567b2903fc6935c
Erwan <erwan.legrand49@gmail.com>
2024-03-26 17:35:08 +0900

    setup hyprland

--- flake.nix
+++ flake.nix
@@ -6,10 +6,9 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     alacritty-theme.url = "github:alexghr/alacritty-theme.nix";
-    hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, hyprland, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       asus-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -19,7 +18,6 @@
             nixpkgs.overlays = [ inputs.alacritty-theme.overlays.default ];
           })
           ./hosts/asus-laptop/configuration.nix
-          hyprland.homeManagerModules.default
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
