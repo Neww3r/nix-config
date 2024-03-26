d059ed305def01257f05d9cf975710829b669e9b
Erwan <erwan.legrand49@gmail.com>
2024-03-26 15:36:25 +0900

    setup stylix

--- hosts/asus-laptop/configuration.nix
+++ hosts/asus-laptop/configuration.nix
@@ -68,10 +68,7 @@ in
   programs.thunar.enable = true;
 
   stylix = {
-    image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    };
+    image = ~/Pictures/wallpapers/wallpaper.png;
     polarity = "dark";
   };
 
