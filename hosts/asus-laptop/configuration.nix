aa3eb9ac7a4491e10d3c26a130b94c5ee4c8139e
Erwan <erwan.legrand49@gmail.com>
2024-08-30 15:57:01 +0200

    update timezone to Europe/Paris

--- hosts/asus-laptop/configuration.nix
+++ hosts/asus-laptop/configuration.nix
@@ -21,7 +21,7 @@ in
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  time.timeZone = "Asia/Seoul";
+  time.timeZone = "Europe/Paris";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
