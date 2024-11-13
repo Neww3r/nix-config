b57d732efa3361a467ebbd381ea0ca1e2bb3fe71
Erwan <erwan.legrand49@gmail.com>
2024-11-14 00:26:25 +0100

    bltooth

--- hosts/asus-laptop/configuration.nix
+++ hosts/asus-laptop/configuration.nix
@@ -55,13 +55,21 @@ in
     mediaKeys.enable = true;
   };
 
+  # Mediakeys for headset
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
+
   # Bluetooth settings
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
     settings = {
       General = {
-        ControllerMode = "bredr";
+        Enable = "Source,Sink,Media,Socket";
       };
     };
   };
