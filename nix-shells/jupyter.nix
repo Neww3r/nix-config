d3bb4d8f25f2a78823fa92d56025b3d1410c6426
Erwan <erwan.legrand49@gmail.com>
2024-11-23 13:22:53 +0100

    add meson pkg

--- nix-shells/jupyter.nix
+++ nix-shells/jupyter.nix
@@ -2,7 +2,11 @@ let
   pkgs = import <nixpkgs> { };
 in
 pkgs.mkShell {
-  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  buildInputs = with pkgs;[
+    portaudio
+  ];
+  LD_LIBRARY_PATH =
+    "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.lib.makeLibraryPath [ pkgs.portaudio ]}";
   shellHook = ''
     . ~/.virtualenvs/masi-py3/bin/activate
     python -m jupyter notebook
--- users/erwan/home.nix
+++ users/erwan/home.nix
@@ -64,6 +64,7 @@ in
     bear
     valgrind
     criterion
+    meson
 
     # Rust Programming
     rustc
