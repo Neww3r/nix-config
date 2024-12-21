df896dc70059b104f7241baf35f5f11309698dea
Erwan <erwan.legrand49@gmail.com>
2024-12-21 23:31:27 +0100

    pipewire settings

--- hosts/asus-laptop/configuration.nix
+++ hosts/asus-laptop/configuration.nix
@@ -61,13 +61,18 @@ in
   };
   services.blueman.enable = true;
 
-  # Pulseaudio settings
-  nixpkgs.config.pulseaudio = true;
-  hardware.pulseaudio = {
-    enable = true;
-    package = pkgs.pulseaudioFull;
-    support32Bit = true; # If compatibility with 32-bit applications is desired.
+  # Pipewire settings
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true; # if not already enabled
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
   };
+
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
