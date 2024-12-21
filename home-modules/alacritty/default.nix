c94e22fb7567e6ab01b27874eb8d8be61b93b765
Erwan <erwan.legrand49@gmail.com>
2024-12-21 18:54:47 +0100

    fix font size

--- home-modules/alacritty/default.nix
+++ home-modules/alacritty/default.nix
@@ -6,7 +6,7 @@
       general.import = [
         pkgs.alacritty-theme.gruvbox_material_medium_dark
       ];
-      font.size = 12;
+      font.size = 10;
       window.opacity = 0.85;
     };
   };
