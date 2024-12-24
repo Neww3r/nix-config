7ab79dd23d50f3d98421c74193464b565105ffdd
erwan <erwan.legrand49@gmailcom>
2024-12-24 15:29:18 +0100

    save volume on shutdown

--- hosts/asus-laptop/configuration.nix
+++ hosts/asus-laptop/configuration.nix
@@ -62,6 +62,7 @@ in
   services.blueman.enable = true;
 
   # Enable sound with pipewire.
+  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
