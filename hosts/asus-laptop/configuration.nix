3731b85e2868f59daa2bb4117dc86bfa1fb5738a
erwan <erwan.legrand49@gmailcom>
2024-12-24 15:30:32 +0100

    remove sound enable option

--- hosts/asus-laptop/configuration.nix
+++ hosts/asus-laptop/configuration.nix
@@ -62,7 +62,6 @@ in
   services.blueman.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
